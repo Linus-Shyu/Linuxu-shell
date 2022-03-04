1bbc351fb0c93fb2c744f7dfdbb2bea2b180c582
Jefferson <83543818+Jefferson-Hsu@users.noreply.github.com>
2022-03-04 13:52:28 +0800

    Add files via upload

---
+++ Linuxu.py
@@ -0,0 +1,213 @@
+#string aphoto
+print(" _   _      _ _    __        __         _     _            .____    .__                        ")
+print("| | | | ___| | | __\\ \\      / /__  _ __| | __| |           |    |   |__| ____  __ _____  _____ __                       ")
+print("| |_| |/ _ \\ | |/ _ \\ \\ /\\ / / _ \\| '__| |/ _` |           |    |   |  |/    \|  |  \  \/  /  |  \                      ") 
+print("|  _  |  __/ | | (_) \\ V  V / (_) | |  | | (_| |           |    |___|  |   |  \  |  />    <|  |  /                      ")
+print("|_| |_|\\___|_|_|\\___/ \\_/\\_/ \\___/|_|  |_|\\__,_|           |_______ \__|___|  /____//__/\_ \____/                       ")
+print("                                                                                                                                   ")
+print("                                                                                                                                   ")
+print("                                                                                                                                   ")
+
+
+#password & user name
+join_key=3
+again_key=4
+name="XuFaxin"
+password="Xinxin080502"
+print("--------------------------------------------------------------------------------------------------------------------------------------------")
+input_name=input("Please type the user name: ")
+print("--------------------------------------------------------------------------------------------------------------------------------------------")
+input_password=input("Please type the password: ")
+print("--------------------------------------------------------------------------------------------------------------------------------------------")
+print("welcome to Linuxu system!!!")
+print("                                                                                                                                            ")
+while(join_key==3):
+    if input_name=="XuFaxin" and input_password=="Xinxin080502":
+        print("                           ")
+        print("                           ")
+    else:
+        print("Bye,you are not user!")
+        break
+
+
+    #command shell
+    command=input("XuFaxin@computer% ")
+
+
+    #root command
+    if(command=="root"):
+        print("                           ")
+        print("you are rooter!")
+        print("                           ")
+        print("But don't be happy too soon")
+        print("                           ")
+        print("-----------------------------------------------------------------------------------------------------------------------------------")
+        print("                                        In the world of Linuxu XuFaxin is god!")
+        print("-----------------------------------------------------------------------------------------------------------------------------------")
+        print("                                                                                                                                   ")    
+        
+
+    #Calculator command
+    if(command=="math"):
+        print("Develop by XuFaxin")
+        counts=3
+        while counts>0:
+            str1=input("First number: ")
+            str2=input("Second number:")
+            X=int(str1)
+            Y=int(str2)
+            print(X+Y)
+            print(X-Y)
+            print(X*Y)
+            print(X/Y)
+            print(X**Y)
+            print(X//Y)
+            break
+    
+
+    #game command
+    if(command=="game"):
+        print("                                              ")
+        print("Welcome to XuFaxin's guess number game!")
+        print("                                              ")
+        print("You have three chances")
+        print("                                              ")
+        print("Guess an integer between 1 and 10")
+        print("                                              ")
+        print("develop by XuFaxin")
+        print("                                              ")
+        print("                                              ")
+        import random
+        answer=random.randint(1,10)
+        counts=3
+        while counts>0:
+            temp=input("Guess a number: ")
+            guess=int(temp)
+            if guess==answer:
+                print("                        ")
+                print("Win")
+                print("                        ")
+                print("Win!!! But no pay! HAHA!")
+            else:
+                if guess>0:
+                    print("             ")
+                    print("Big!")
+                    print("             ")
+                else:
+                    print("      ")
+                    print("small!")
+            counts=counts-1
+            
+
+    #clear command
+    if(command=="clear"):
+        print("                                                                                                                    ")
+        print("                                                                                                                    ")
+        print("                                                                                                                    ")
+        print("                                                                                                                    ")
+        print("                                                                                                                    ")
+        print("                                                                                                                    ")
+        print("                                                                                                                    ")
+        print("                                                                                                                    ")
+        print("                                                                                                                    ")
+        print("                                                                                                                    ")
+        print("                                                                                                                    ")
+        print("                                                                                                                    ")
+        print("                                                                                                                    ")
+        print("                                                                                                                    ")
+        print("                                                                                                                    ")
+        print("                                                                                                                    ")
+        print("                                                                                                                    ")
+        print("                                                                                                                    ")
+        print("                                                                                                                    ")
+        print("                                                                                                                    ")
+        print("                                                                                                                    ")
+        print("                                                                                                                    ")
+        print("                                                                                                                    ")
+        print("                                                                                                                    ")
+        print("                                                                                                                    ")
+        print("                                                                                                                    ")
+        print("                                                                                                                    ")
+        print("                                                                                                                    ")
+        print("                                                                                                                    ")
+        print("                                                                                                                    ")
+        print("                                                                                                                    ")
+        print("                                                                                                                    ")
+        print("                                                                                                                    ")
+        print("                                                                                                                    ")
+        print("                                                                                                                    ")
+        print("                                                                                                                    ")
+        print("                                                                                                                    ")
+        print("                                                                                                                    ")
+        print("                                                                                                                    ")
+        print("                                                                                                                    ")
+        print("                                                                                                                    ")
+        print("                                                                                                                    ")
+        print("                                                                                                                    ")
+        print("                                                                                                                    ")
+        print("                                                                                                                    ")
+        print("                                                                                                                    ")
+        print("                                                                                                                    ")
+        print("                                                                                                                    ")
+        print("                                                                                                                    ")
+        print("                                                                                                                    ")
+        print("                                                                                                                    ")
+        print("                                                                                                                    ")
+        print("                                                                                                                    ")
+        print("                                                                                                                    ")
+        print("                                                                                                                    ")
+        print("                                                                                                                    ")
+        print("                                                                                                                    ")
+        print("                                                                                                                    ")
+        print("                                                                                                                    ")
+        print("                                                                                                                    ")
+        print("                                                                                                                    ")
+        print("                                                                                                                    ")
+        print("                                                                                                                    ")
+        print("                                                                                                                    ")
+        print("                                                                                                                    ")
+        print("                                                                                                                    ")
+        print("                                                                                                                    ")
+        print("                                                                                                                    ")
+        print("                                                                                                                    ")
+        print("                                                                                                                    ")
+        print("                                                                                                                    ")
+        print("                                                                                                                    ")
+        print("                                                                                                                    ")
+        print("                                                                                                                    ")
+        print("                                                                                                                    ")
+        print("                                                                                                                    ")
+        print("                                                                                                                    ")
+        print("                                                                                                                    ")
+        print("                                                                                                                    ")
+        print("                                                                                                                    ")
+        print("                                                                                                                    ")
+        print("                                                                                                                    ")
+        print("                                                                                                                    ")
+        print("                                                                                                                    ")
+        print("                                                                                                                    ")
+        print("                                                                                                                    ")
+        print("                                                                                                                    ")
+        print("                                                                                                                    ")
+        print("                                                                                                                    ")
+        print("                                                                                                                    ")
+        print("                                                                                                                    ")
+        print("                                                                                                                    ")
+        print("                                                                                                                    ")
+        print("                                                                                                                    ")
+        print("                                                                                                                    ")
+        print("                                                                                                                    ")
+        
+        
+    #list command
+    if(command=="ls"):
+            print("-------------------------------------------------------------------------------------------------------------------------------")
+            print(" ||game|| ||math||   ")
+            print("-------------------------------------------------------------------------------------------------------------------------------")
+
+            
+    #exit command
+    if(command=="exit"):
+        print("                                                                                                                    ")
+        print("See you again!")
+        break
